0a72ca2a1f67446fac87b4a4a8384e6fe68a43d7
Koraboevvv <bexruzkarabaev@gmail.com>
2025-04-14 16:11:28 +0500

    a

--- script.js
+++ script.js
@@ -1,121 +1,121 @@
-// {
-//     //M-1
-//     var a=prompt("a sonni kiriting")
-//     console.log( Number ( a ) * 4 );
+{
+    //M-1
+    var a=prompt("a sonni kiriting")
+    console.log( Number ( a ) * 4 );
     
-// }
-// //.........................................................//
-
-// {
-//     //M-2
-//     var a=prompt("a sonni kiriting")
-//     console.log( Number ( a ) **2 );
-    
-// }
-// //.........................................................//
-
-// {
-//     //M-3
-//     var a=prompt("a= ")
-//     var b=prompt("b= ")
-//     var geo= Math.sqrt( a * b )
-//     console.log(geo);
-// }
-// //.........................................................//
-
-// {
-//     //M-4
-//     var d=prompt("d kiriting")
-//     const pi=3.14;
-//     console.log(Number( d ) * pi );
-// }
-// //.........................................................//
-
-// {
-//     //M-5
-//     var a=prompt("a kiriting")
-//     const v=a*a*a;
-//     const akv=a*a
-//     const s=6*a**2;
-//     console.log(v);
-//     console.log(akv);
-//     console.log(s);
-// }
-// //.........................................................//
-
-// {
-//     //M-6
-//     var a=prompt("a kiriting")
-//     var b=prompt("b kiriting")
-//     var c=prompt("c kiriting")
-//     console.log(Number ( a * b * c ));
-//     const s= 2 * (a * b + b * c + a * c);
-// }
-// //.........................................................//
-
-// {
-//     //M-7
-//     var r=prompt("Radiusni kiriting")
-//     const i=2*3.14*r;
-//     const pi=3.14;
-//     const rkv=r*r;
-//     const s=pi*rkv;
-//     console.log(i);
-//     console.log(s);
-    
-// }
-// //.........................................................//
-
-// {
-//     //M-8
-//     var a=prompt("a kiriting")
-//     var b=prompt("b kiriting")
-//     console.log(Number ( ( a + b ) / 2 ));
-    
-// }
-// //.........................................................//
-
-// {
-//     //M-9
-//     var a=prompt("a kiriting")
-//     var b=prompt("b kiriting")
-//     var ort= Math.sqrt (a*b)
-//     console.log(ort);
-    
-// }
-// //.........................................................//
-
-// {
-//     //M-10
-//     var a=prompt("a kiriting")
-//     var b=prompt("b kiriting")
-//     const yig=a+b;
-//     const kop=a*b;
-//     const akv=a*a
-//     const bkv=b*b
-//     console.log(yig);
-//     console.log(kop);
-//     console.log(akv);
-//     console.log(bkv);
-    
-// }
-// //.........................................................//
-
-// {
-//     //M-11
-//     var a=prompt("a kiriting")
-//     var b=prompt("b kiriting")
-//     console.log(Number(a)+Number(b));
-//     console.log(Number(a)-Number(b));
-//     const r=a-b;
-//     var absA= Math.abs(a)
-//     var absB= Math.abs(b)
-//     // console.log(s);
-//     // console.log(r);
-//     console.log(absA);
-//     console.log(absB);
-// }
+}
 //.........................................................//
+
+{
+    //M-2
+    var a=prompt("a sonni kiriting")
+    console.log( Number ( a ) **2 );
+    
+}
+//.........................................................//
+
+{
+    //M-3
+    var a=prompt("a= ")
+    var b=prompt("b= ")
+    var geo= Math.sqrt( a * b )
+    console.log(geo);
+}
+//.........................................................//
+
+{
+    //M-4
+    var d=prompt("d kiriting")
+    const pi=3.14;
+    console.log(Number( d ) * pi );
+}
+//.........................................................//
+
+{
+    //M-5
+    var a=prompt("a kiriting")
+    const v=a*a*a;
+    const akv=a*a
+    const s=6*a**2;
+    console.log(v);
+    console.log(akv);
+    console.log(s);
+}
+//.........................................................//
+
+{
+    //M-6
+    var a=prompt("a kiriting")
+    var b=prompt("b kiriting")
+    var c=prompt("c kiriting")
+    console.log(Number ( a * b * c ));
+    const s= 2 * (a * b + b * c + a * c);
+}
+//.........................................................//
+
+{
+    //M-7
+    var r=prompt("Radiusni kiriting")
+    const i=2*3.14*r;
+    const pi=3.14;
+    const rkv=r*r;
+    const s=pi*rkv;
+    console.log(i);
+    console.log(s);
+    
+}
+//.........................................................//
+
+{
+    //M-8
+    var a=prompt("a kiriting")
+    var b=prompt("b kiriting")
+    console.log(Number ( ( a + b ) / 2 ));
+    
+}
+//.........................................................//
+
+{
+    //M-9
+    var a=prompt("a kiriting")
+    var b=prompt("b kiriting")
+    var ort= Math.sqrt (a*b)
+    console.log(ort);
+    
+}
+//.........................................................//
+
+{
+    //M-10
+    var a=prompt("a kiriting")
+    var b=prompt("b kiriting")
+    const yig=a+b;
+    const kop=a*b;
+    const akv=a*a
+    const bkv=b*b
+    console.log(yig);
+    console.log(kop);
+    console.log(akv);
+    console.log(bkv);
+    
+}
+//.........................................................//
+
+{
+    //M-11
+    var a=prompt("a kiriting")
+    var b=prompt("b kiriting")
+    console.log(Number(a)+Number(b));
+    console.log(Number(a)-Number(b));
+    const r=a-b;
+    var absA= Math.abs(a)
+    var absB= Math.abs(b)
+    // console.log(s);
+    // console.log(r);
+    console.log(absA);
+    console.log(absB);
+}
+.........................................................//
 {
     //M-12
     var a=prompt("a kiriting")
